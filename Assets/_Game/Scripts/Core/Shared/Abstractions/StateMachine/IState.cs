07696628f5312d09b7d7bb86e3799a9e6f8b51b7
WolfRedPlay <126975145+WolfRedPlay@users.noreply.github.com>
2025-09-21 19:40:44 +0200

    Abstractions
    Some abstractions for state machines for, also abstraction for movement controller and animator controller.
    
    Implemented event bus.

---
+++ Assets/_Game/Scripts/Core/Shared/Abstractions/StateMachine/IState.cs
@@ -0,0 +1,23 @@
+﻿namespace Core.Shared.StateMachine
+{
+    /// <summary>
+    /// Interface for states which are used in state managers.
+    /// </summary>
+    public interface IState
+    {
+        /// <summary>
+        /// This method is called on start of the state.
+        /// </summary>
+        public void OnStart();
+
+        /// <summary>
+        /// This method is called on each frame of the state.
+        /// </summary>
+        public void OnUpdate();
+      
+        /// <summary>
+        /// This method is called on stop of the state.
+        /// </summary>
+        public void OnStop();
+    }
+}
