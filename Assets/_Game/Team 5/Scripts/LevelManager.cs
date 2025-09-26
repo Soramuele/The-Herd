51b24a930db2e2aca4ad0ba185fdbea0edf99a8e
jariolyslager <jariolyslager@gmail.com>
2025-09-26 09:15:15 +0200

    Basic folder structure and assets for team 5.

---
+++ Assets/_Game/Team 5/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    /* This script will contain the main logic that's needed for all events occurring during the level playtime. */
+
+    void Initialize()
+    {
+        // Method for initializing the level.
+    }
+}
