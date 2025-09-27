c9dcc2ab1f57ee688352c3a93b8149fa38057ec0
jariolyslager <jariolyslager@gmail.com>
2025-09-27 14:10:37 +0200

    Added (empty) script file for scare-event

---
+++ Assets/_Game/Team 5/Scripts/PeakScare.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PeakScare : MonoBehaviour
+{
+    /* This script will contain the code for the "peak scare"-event that will run upon reaching a certain trigger. */
+
+    void Scare()
+    {
+        // Method for the logic for the peak scare.
+    }
+}
